a0394e513c637a452a4fbb2759f62b39028e1b3f
Brian Mafu <brianmafu@gmail.com>
2019-07-10 13:33:42 +0200

    added staticfiles

--- osmosis_music_api/settings.py
+++ osmosis_music_api/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(BASE_DIR, 'static')
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
