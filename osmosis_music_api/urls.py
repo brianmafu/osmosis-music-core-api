e83060fc286015898b0e45be3502caa19f157cc3
Brian Mafu <brian@aidence.com>
2022-09-09 00:37:57 +0200

    updated api url for api path

--- osmosis_music_api/urls.py
+++ osmosis_music_api/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest')),
     path('api-token-auth', obtain_jwt_token),
-    path('API/', include(('music_core.urls', 'music_core_api'), namespace='music_core_api')),
+    path('api/', include(('music_core.urls', 'music_core_api'), namespace='music_core_api')),
 
 ]
